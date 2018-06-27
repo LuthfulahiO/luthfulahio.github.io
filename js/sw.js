ee0e92d5010f76478b269282ec98256458924a43
luteeph01 <luteeph@gmail.com>
2018-06-27 21:41:29 +0100

    fixing service worker

--- js/sw.js
+++ js/sw.js
@@ -1,29 +1,82 @@
-var cacheName = 'currency-converter';
-var filesToCache = [
-    '/',
-    '../index.html',
-    '../css/bootstrap.css'
-];
+// var cacheName = 'currency-converter';
+// var filesToCache = [
+//     '/',
+//     '../index.html',
+//     '../css/bootstrap.css'
+// ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntill(
+// self.addEventListener('install', function (e) {
+//     e.waitUntill(
+//         caches.open(cacheName).then(function (cache) {
+//             return cache.addAll(filesToCache);
+//         })
+//     );
+// });
+
+// self.addEventListener('activate', event => {
+//     event.waitUntill(self.clients.claim());
+// });
+
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request, {
+//             ignoreSearch: true
+//         })
+//         .then(response => {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
+let appName = 'currency-converter';
+let version = 'v1';
+let cacheName = appName + version;
+
+self.addEventListener('fetch', function (event) {
+    console.log('fetch request : ' + event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + event.request.url);
+                return request;
+            } else {
+                console.log('file is not cached, fetching : ' + event.request.url);
+                return fetch(event.request);
+            }
+        })
+    )
+})
+
+// Cache resources
+self.addEventListener('install', function (event) {
+    event.waitUntil(
         caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
+            console.log('installing cache : ' + cacheName);
+            return cache.addAll([
+                '/',
+                '../index.html',
+                '../css/bootstrap.css',
+                '../img/icon.png'
+            ]);
         })
     );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntill(self.clients.claim());
-});
+// Delete outdated caches
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(appName);
+            });
+            // add current cache name to white list
+            cacheWhitelist.push(cacheName);
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, {
-            ignoreSearch: true
-        })
-        .then(response => {
-            return response || fetch(event.request);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }))
         })
     );
 });
