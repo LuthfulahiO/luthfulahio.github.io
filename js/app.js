973e75ae0534840c521ddf09fecab949aa813f07
luteeph01 <luteeph@gmail.com>
2018-06-27 19:25:31 +0100

    did conversion for online only

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('#userInput');
+const result = document.getElementById('result');
+const fillin = document.getElementsByClassName('op');
+const autoConvert = () => {
+    const from = document.querySelector('#from').value;
+    const to = document.querySelector('#to').value;
+    let valueToBeConverted = input.value;
+    const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+            const keys = Object.entries(res);
+            const xxx = valueToBeConverted * keys[0][1].val;
+            result.innerText = xxx.toLocaleString("us");
+        });
+}
+const url = "https://free.currencyconverterapi.com/api/v5/countries"
+fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+        const currencies = res.results;
+        const currenciesArray = Object.entries(currencies);
+        currenciesArray.forEach(element => {
+            let country = element[1].name;
+            let id = element[1].currencyId;
+            let name = element[1].currencyName;
+            let symbolx = element[1].currencySymbol;
+            if (element[1].currencySymbol == undefined) {
+                symbolx = "";
+            }
+            fillin[0].innerHTML += `<option value="${id}">${country} - ${name} - ${symbolx}</option>`;
+            fillin[1].innerHTML += `<option value="${id}">${country} - ${name} - ${symbolx}</option>`;
+        });
+    });
