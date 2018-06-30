756eb56a5131140caeb583e2e8825b192deb7dd2
luteeph01 <luteeph@gmail.com>
2018-06-30 08:01:05 +0100

    I am tired of commiting please work tomorrow is deadline

--- sw.js
+++ sw.js
@@ -1,30 +1,49 @@
-let cacheName = 'currency-converter';
+let cache = "cc";
+let version = "1.1.0";
+let cacheName = `${cache}_${version}`;
 let filesToCache = [
-    '/',
-    '/index.html',
-    '/css/bootstrap.css',
-    'img/icon.png'
+    "/currencyconverter/",
+    "/currencyconverter/index.html",
+    "./manifest.json",
+    "./img/icon.png",
+    "./css/bootstrap.css",
+    "./js/app.js",
+    // "./js/idb.js",
+    "https://free.currencyconverterapi.com/api/v5/currencies"
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntill(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(filesToCache);
+
+
+self.addEventListener("install", event => {
+    console.log("[Service Worker] installing ");
+
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log("[Service Worker] caching all files");
+            cache.addAll(filesToCache);
+        }).then(() => self.skipWaiting()).catch(err => console.log("error occured in caching files ==> ", err))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request)
         })
     );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntill(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, {
-            ignoreSearch: true
-        })
-        .then(response => {
-            return response || fetch(event.request);
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            Promise.all(
+                keyList.map(key => {
+                    if (key !== cacheName) {
+                        caches.delete(key);
+                        console.log(`deleted ${key}`)
+                    }
+                })
+            );
         })
     );
 });
