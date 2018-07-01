56a00019e81a40c13954b0d2e00d93ae230b39f6
luteeph01 <luteeph@gmail.com>
2018-07-01 18:16:02 +0100

    implemented indexdb

--- js/app.js
+++ js/app.js
@@ -1,19 +1,82 @@
 const input = document.querySelector('#userInput');
 const result = document.getElementById('result');
 const fillin = document.getElementsByClassName('op');
+
+//database created here
+let dbPromise = idb.open("currencyconverter", 1, upgradeDB => {
+    switch (upgradeDB.oldVersion) {
+        case 0:
+            let rateStore = upgradeDB.createObjectStore("rates");
+    }
+});
+
+// a global function to be called when the user converts
+
+let storeRate = (query, rate) => {
+    let query_currencies = query.split("_");
+
+    dbPromise
+        .then(db => {
+            let tx = db.transaction("rates", "readwrite");
+            let rateStore = tx.objectStore("rates");
+
+            if (query_currencies[0] == query_currencies[1]) {
+                rateStore.put(parseFloat(rate).toFixed(6), query);
+                return tx.complete;
+            }
+
+            rateStore.put(parseFloat(rate).toFixed(6), query);
+            rateStore.put(
+                parseFloat(1 / rate).toFixed(6),
+                `${query_currencies[1]}_${query_currencies[0]}`
+            );
+            return tx.complete;
+        })
+        .then(() => console.log("Wow I just stored conversion rate for", query))
+        .catch((err) => console.log("oops there is an error and here it is", err));
+};
+
 const autoConvert = () => {
     const from = document.querySelector('#from').value;
     const to = document.querySelector('#to').value;
+    const errormessage = document.querySelector('#errormessages');
+    const successmessage = document.querySelector('#successmessage');
     let valueToBeConverted = input.value;
-    const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((res) => {
-            const keys = Object.entries(res);
-            const xxx = Number(valueToBeConverted) * keys[0][1].val;
+    let query = `${from}_${to}`;
+    let rate;
+    if (!from || !to) {
+        errormessage.innerText = `Select both currencies you wil be converting`;
+        return;
+    }
+    dbPromise
+        .then(db => {
+            let tx = db.transaction("rates");
+            let rateStore = tx.objectStore("rates");
+            return rateStore.get(query);
+        })
+        .then(rate => {
+            if (!rate) {
+                const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+                fetch(url)
+                    .then((res) => res.json())
+                    .then((res) => {
+                        const keys = Object.entries(res);
+                        rate = keys[0][1].val;
+                        const xxx = Number(valueToBeConverted) * rate;
+                        result.innerText = xxx.toLocaleString("us");
+                        successmessage.innerText = `1 ${from} is equivalent to 1 ${to}`;
+                        storeRate(query, rate);
+                    })
+                    .catch(err => {
+                        errormessage.innerText = `Oops! seem you are not in good terms with the internet and unfortunately I'm yet to save conversion for ${query}`;
+                    })
+                return;
+            }
+            const xxx = Number(valueToBeConverted) * rate;
             result.innerText = xxx.toLocaleString("us");
+            successmessage.innerText = `1 ${from} is equivalent to 1 ${to}`;
         });
-}
+};
 const url = "https://free.currencyconverterapi.com/api/v5/currencies";
 fetch(url)
     .then((res) => res.json())
